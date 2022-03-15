1e8f84b6783d14959f9d801681b6235f9de8d492
JeffAllison <itsjallison@gmail.com>
2022-03-15 16:06:28 -0700

    updated statslistEL text content to indclude objects within object

--- render-amps.js
+++ render-amps.js
@@ -1,13 +1,10 @@
 export function renderAmpEl(amps) {
-  // <!-- <div class="amps">
+  //  <!-- <div class="amps">
   // <h3>Name: Fender Twin Reverb</h3>
   // <p>year: 1963</p>
   // <p>Manufacturer: Fender</p>
-  // <ul class = "stats">
-  /* <li>85 watts</li> */
-  // {* <li>tube</li> */ 
-  /* <li>combo</li> */
-  /* </div> --> */
+  // <p>stats</p>
+  // </div> -->
 
   const ampsEl = document.createElement('div');
   const ampsNameEl = document.createElement('h3');
@@ -20,13 +17,7 @@ export function renderAmpEl(amps) {
   ampsNameEl.textContent = (amps.name);
   yearEl.textContent = `Year: ${amps.year}`;
   manufacturerEl.textContent = `Manufacturer: ${amps.manufacturer}`;
-
-  for (let stat of amps.stats) {
-    const statsEl = document.createElement('li');
-    statsEl.textContent = stat;
-
-    statsListEl.append(statsEl);
-  }
+  statsListEl.textContent = `Wattage: ${amps.stats.wattage}. Type: ${amps.stats.type}. Format: ${amps.stats.format}`;
 
   ampsEl.append(ampsNameEl, yearEl, manufacturerEl, statsListEl);
 
