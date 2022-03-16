c6f840c69799845eebc7b473d52749c418773f79
JeffAllison <itsjallison@gmail.com>
2022-03-15 17:36:38 -0700

    ACP to resolve eslint errors

--- whales-data.js
+++ whales-data.js
@@ -1,20 +1,20 @@
 export const whales = [
-    {
-        name: 'Blue Whale',
-        lengthInFeet: 80,
-        weightInLbs: 300000,
-        diet: 'krill'
-    },
-    {
-        name: 'Right Whale',
-        lengthInFeet: 46,
-        weightInLbs: 100000,
-        diet: 'zooplankton'
-    },
-    {
-        name: 'Humpback Whale',
-        lengthInFeet: 50,
-        weightInLbs: 60000,
-        diet: 'krill'
-    }
+  {
+    name: 'Blue Whale',
+    lengthInFeet: 80,
+    weightInLbs: 300000,
+    diet: 'krill'
+  },
+  {
+    name: 'Right Whale',
+    lengthInFeet: 46,
+    weightInLbs: 100000,
+    diet: 'zooplankton'
+  },
+  {
+    name: 'Humpback Whale',
+    lengthInFeet: 50,
+    weightInLbs: 60000,
+    diet: 'krill'
+  }
 ];
